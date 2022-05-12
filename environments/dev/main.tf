bf7318f551b1839895eae5d477dd1e89d3496a0e
SANAN AHMADOV <senanahmedli@yahoo.com>
2022-05-12 13:53:28 +0200

    change the name of output 2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,10 +1,10 @@
-resource "google_project" "landing-zone-dev" {
+resource "google_project" "landing-zone-dev-name" {
   name       = "landing-zone-dev"
   project_id = "landing-zone-dev"
   org_id     = "1045437797549"
 }
 
 provider "google" {
-  project = google_project.landing-zone-dev.id
+  project = google_project.landing-zone-dev-name.id
   region  = var.region
 }

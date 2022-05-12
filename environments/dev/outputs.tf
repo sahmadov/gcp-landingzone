6db144e254c0f51a4c195adf9927318141a9656e
SANAN AHMADOV <senanahmedli@yahoo.com>
2022-05-12 13:46:03 +0200

    change the name of output

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,4 +1,4 @@
 output "project_name" {
-  description = "Name of created Project"
+  description = "Name of created Project is listed below"
   value       = google_project.landing-zone-dev.name
 }

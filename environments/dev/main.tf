a9f176b18cf48964ef9199572ea0294add6f97a9
SANAN AHMADOV <senanahmedli@yahoo.com>
2022-05-12 14:26:07 +0200

    e

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,10 +1,9 @@
+provider "google" {
+  region  = var.region
+}
+
 resource "google_project" "landing-zone-dev-name" {
   name       = "landing-zone-dev"
   project_id = "landing-zone-dev"
   org_id     = "1045437797549"
-}
-
-provider "google" {
-  project = google_project.landing-zone-dev-name.id
-  region  = var.region
 }
